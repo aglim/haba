e3809d687615b72e9e4a740402ddb9940ab4349a
Ramil Agliamzanov <ra@li2.local>
2020-11-30 15:24:29 +0300

    v2

--- app.py
+++ app.py
@@ -2,12 +2,26 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/haba')
 def hello_world():
 
-    s = ["Hello, Haba!",
-        "Hello, Arsen!",
-        "Hello, Karim!"]
+	s = ["Hello, Haba!",
+		"Hello, Arsen!",
+		"Hello, Karim!"]
 
-    out = "<pre>{}</pre>".format("\n".join(s))
-    return out
+	out = "<pre>{}</pre>".format("\n".join(s))
+	return out
+
+
+@app.route('/user/<username>/<iq>')
+def hello_haba(username, iq):
+
+	s = []
+
+	for i in range(100):
+		s.append(username+str(iq*2)+"iq")
+
+	out = "<pre>{}</pre>".format("\n".join(s))
+	return out
+
+app.debug = True
+app.run()
